44190e8a7e5dfadc58a9814b0596e8e4f85cb323
Roy O. Jacome <rjacomejr96@aol.com>
2018-11-26 12:48:40 -0500

    Added tlds server info for client

--- Client.py
+++ Client.py
@@ -6,8 +6,8 @@ def connectClient():
 
     try:
         rsClientSocket = aSocket.socket(aSocket.AF_INET, aSocket.SOCK_STREAM)
-        tlds1Socket = aSocket.socket(aSocket.AF_INET, aSocket.SOCK_STREAM)
-        tlds2Socket = aSocket.socket(aSocket.AF_INET, aSocket.SOCK_STREAM)
+        tlds1SocketServer = aSocket.socket(aSocket.AF_INET, aSocket.SOCK_STREAM)
+        tlds2SocketServer = aSocket.socket(aSocket.AF_INET, aSocket.SOCK_STREAM)
         print("[C]: Successfully created sockets")
     except aSocket.error as err:
         print("Socket open error: {0} \n".format(err))
@@ -15,14 +15,27 @@ def connectClient():
 
     tlds1ServerName = "TLDS1"
     tlds2ServerName = "TLDS2"
+    tlds1ServerHostName = ''
+    tlds2ServerHostName = ''
 
     rsPort = 6000
     #rsHostName = rootServerName
     rsHostName = ""
     rsAddr = aSocket.gethostbyname(rsHostName)
     rsSocketConnection = (rsAddr, rsPort)
-
     rsClientSocket.connect(rsSocketConnection)
+
+    tlds1ServerPort = 7500
+    tlds1ServerAddr = aSocket.gethostbyname(tlds1ServerHostName)
+    #tlds1ServerConnection = (tlds1ServerAddr, tlds1ServerPort)
+    tlds1ServerConnection = ('', tlds1ServerPort)
+    tlds1SocketServer.connect(tlds1ServerConnection)
+
+    tlds2ServerPort = 8500
+    tlds2ServerAddr = aSocket.gethostbyname(tlds2ServerHostName)
+    #tlds2ServerConnection = (tlds2ServerAddr, tlds2ServerPort)
+    tlds2ServerConnection = ('', tlds2ServerPort)
+    tlds2SocketServer.connect(tlds2ServerConnection)
 
     #hnsFile = sys.argv[2]
     hnsFile = "PROJ3-HNS.txt"
@@ -56,15 +69,14 @@ def connectClient():
 
                     if(serverResult == tlds1ServerName):
                         print("[C]: Connect to TLDS1")
-                        tlds1Socket.send(hostName.encode('utf-8'))
-                        serverResult = tlds1Socket.recv(1024).decode('utf-8')
+                        tlds1SocketServer.send(hostName.encode('utf-8'))
+                        serverResult = tlds1SocketServer.recv(1024).decode
                     elif(serverResult == tlds2ServerName):
                         print("[C]: Connect to TLDS2")
-                        tlds2Socket.send(hostName.encode('utf-8'))
-                        serverResult = tlds2Socket.recv(1024).decode('utf-8')
+                        tlds2SocketServer.send(hostName.encode('utf-8'))
+                        tlds2SocketServer.recv(1024).decode('utf-8')
                     else:
                         print("[C]: Client does not connect to anyone")
-                
 
                     # Write result to file
                     writeFile.write(serverResult + "\n")
