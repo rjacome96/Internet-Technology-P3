f5e187e93584b1550d9842256d00fb4673ec9bdf
Roy O. Jacome <rjacomejr96@aol.com>
2018-11-26 23:17:24 -0500

    Using given test files

--- AS.py
+++ AS.py
@@ -14,12 +14,17 @@ def AuthenticationServer():
     except aSocket.error as err:
         print("Socket open error: {0} \n".format(err))
         return
-
+    
+    # The names that the TLDS servers will go by
+    # (If they will not be reffered to by their host name)
     tlds1ServerName = "TLDS1"
     tlds2ServerName = "TLDS2"
-    tlds1ServerHostName = ''
-    tlds2ServerHostName = ''
 
+    # The names of the TLDS servers, will be given as input later
+    tlds1ServerHostName = "kill.cs.rutgers.edu"
+    tlds2ServerHostName = "grep.cs.rutgers.edu"
+
+    # Easier to send to TLDS whether their digests matched with the client's
     matchedMessage = "Matched".encode('utf-8')
     noMatchMessage = "No Match".encode('utf-8')
 
@@ -32,18 +37,18 @@ def AuthenticationServer():
     # Connect to tlds1 server
     tlds1ServerPort = 7000
     tlds1ServerAddr = aSocket.gethostbyname(tlds1ServerHostName)
-    #tlds1ServerConnection = (tlds1ServerAddr, tlds1ServerPort)
-    tlds1ServerConnection = ('', tlds1ServerPort)
+    tlds1ServerConnection = (tlds1ServerAddr, tlds1ServerPort)
+    #tlds1ServerConnection = ('', tlds1ServerPort)
     tlds1SocketServer.connect(tlds1ServerConnection)
 
     # Connect to tlds2 server
     tlds2ServerPort = 8000
     tlds2ServerAddr = aSocket.gethostbyname(tlds2ServerHostName)
-    #tlds2ServerConnection = (tlds2ServerAddr, tlds2ServerPort)
-    tlds2ServerConnection = ('', tlds2ServerPort)
+    tlds2ServerConnection = (tlds2ServerAddr, tlds2ServerPort)
+    #tlds2ServerConnection = ('', tlds2ServerPort)
     tlds2SocketServer.connect(tlds2ServerConnection)
 
-    
+
     # Have socket listen on the port 6000
     clientSocketServer.listen(1)
     print("[AS]: Listening for one connection on port 6000...")
@@ -54,6 +59,7 @@ def AuthenticationServer():
     # Once connected, service the client until the end
     while True:
 
+        # Obtain client's digest and challenge string
         clientInfo = clientSocket.recv(1024).decode('utf-8')
         
         if not clientInfo:
@@ -61,23 +67,25 @@ def AuthenticationServer():
 
         print("[AS]: Recieved from client: {}".format(clientInfo))
 
+        # Distinguish client's digest and challenge string
         clientInfo = clientInfo.split()
-        
         clientChallengeString = clientInfo[0]
         clientDigestHex = clientInfo[1]
-
         print("[AS]: Client challenge string: {}".format(clientChallengeString))
         print("[AS]: Client digest: {}".format(clientDigestHex))
 
+        # Send client's challenge string to both TLDS servers
         tlds1SocketServer.send(clientChallengeString.encode('utf-8'))
         tlds2SocketServer.send(clientChallengeString.encode('utf-8'))
         
+        # Receive the servers' resluting digests in hex form
         tlds1DigestHex = tlds1SocketServer.recv(1024).decode('utf-8')
         tlds2DigestHex = tlds2SocketServer.recv(1024).decode('utf-8')
         print("[AS]: {}".format(tlds1DigestHex))
         print("[AS]: {}".format(tlds2DigestHex))
 
 
+        # Compare client's digest with both servers' digests
         # Client is authenticated for TLDS1
         if(hmac.compare_digest(clientDigestHex, tlds1DigestHex)):
             print("[AS]: Client authorized for TLDS1")
--- Client.py
+++ Client.py
@@ -4,6 +4,7 @@ import hmac
 
 def connectClient():
 
+    # Attempt to create three sockets for the three servers
     try:
         rsClientSocket = aSocket.socket(aSocket.AF_INET, aSocket.SOCK_STREAM)
         tlds1SocketServer = aSocket.socket(aSocket.AF_INET, aSocket.SOCK_STREAM)
@@ -13,33 +14,39 @@ def connectClient():
         print("Socket open error: {0} \n".format(err))
         return
 
+    # The names that the TLDS servers will go by
+    # (If they will not be reffered to by their host name)
     tlds1ServerName = "TLDS1"
     tlds2ServerName = "TLDS2"
-    tlds1ServerHostName = ''
-    tlds2ServerHostName = ''
 
+    # The host names of AS and TLDS servers, will be given as input later most likely
+    tlds1ServerHostName = "kill.cs.rutgers.edu"
+    tlds2ServerHostName = "grep.cs.rutgers.edu"
+    rsHostName = "facade.cs.rutgers.edu"
+
+    # Create the connections for the servers
     rsPort = 6000
-    #rsHostName = rootServerName
-    rsHostName = ""
     rsAddr = aSocket.gethostbyname(rsHostName)
     rsSocketConnection = (rsAddr, rsPort)
+    #rsSocketConnection = ('', rsPort)
     rsClientSocket.connect(rsSocketConnection)
 
     tlds1ServerPort = 7500
     tlds1ServerAddr = aSocket.gethostbyname(tlds1ServerHostName)
-    #tlds1ServerConnection = (tlds1ServerAddr, tlds1ServerPort)
-    tlds1ServerConnection = ('', tlds1ServerPort)
+    tlds1ServerConnection = (tlds1ServerAddr, tlds1ServerPort)
+    #tlds1ServerConnection = ('', tlds1ServerPort)
     tlds1SocketServer.connect(tlds1ServerConnection)
 
     tlds2ServerPort = 8500
     tlds2ServerAddr = aSocket.gethostbyname(tlds2ServerHostName)
-    #tlds2ServerConnection = (tlds2ServerAddr, tlds2ServerPort)
-    tlds2ServerConnection = ('', tlds2ServerPort)
+    tlds2ServerConnection = (tlds2ServerAddr, tlds2ServerPort)
+    #tlds2ServerConnection = ('', tlds2ServerPort)
     tlds2SocketServer.connect(tlds2ServerConnection)
 
     #hnsFile = sys.argv[2]
     hnsFile = "PROJ3-HNS.txt"
 
+    # Begin connecting to servers to satisfy requests
     try:
         with open(hnsFile, "r") as readFile:
             with open("RESOLVED.txt", "w") as writeFile:
@@ -52,21 +59,21 @@ def connectClient():
                     clientChallenge = infoLine[1].rstrip()
                     hostName = infoLine[2].rstrip()
 
+                    # Create the digest and digest hexadecimal by using the given key and challenge string
                     clientDigest = hmac.new(clientKey.encode(), clientChallenge.encode('utf-8'))
-
                     clientDigestHex = clientDigest.hexdigest()
 
+                    # Send challenge and digest to Authentication Server
                     sendToRootServer = clientChallenge + " " + clientDigestHex
-
                     print("[C]: {}".format(sendToRootServer))
                     
-
                     # First contact to RS server
                     rsClientSocket.send(sendToRootServer.encode('utf-8'))
 
                     # Get resulting tlds server from RS server
                     serverResult = rsClientSocket.recv(1024).decode('utf-8')
 
+                    # Determine which TLDS server to connect given by RS server
                     if(serverResult == tlds1ServerName):
                         print("[C]: Connect to TLDS1")
                         tlds1SocketServer.send(hostName.encode('utf-8'))
